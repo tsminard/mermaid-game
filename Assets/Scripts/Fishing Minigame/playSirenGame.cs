b8d2e8fd8e2b5e6e3dcfa5fbb36a3c61f1a36afa
tsminard <t.sage.minard@gmail.com>
2025-03-04 14:14:40 -0500

    hooked up Up + Down actions in siren minigame

--- Assets/Scripts/Fishing Minigame/playSirenGame.cs
+++ Assets/Scripts/Fishing Minigame/playSirenGame.cs
@@ -4,12 +4,21 @@ public class playSirenGame : playFishingGame
 {
     private string upBarTag = "UpBar";
     private string downBarTag = "DownBar";
-    private bool isUpSide; // we also have isLeftSide in parent class
+    private bool isUpSide = false; // we also have isLeftSide in parent class
+    private bool isDownSide = false; // we need both of these or else (false, false) will apply to both rightside and downside
 
     public override void Awake()
     {
         base.Awake(); // call parent Awake() to set left / right colliders
         setUpDownColliders();
+        if(isUpSide && !isDownSide)
+        {
+            inputAction = playerInput.actions.FindAction("CastUp");
+        }
+        else if (!isUpSide && isDownSide)
+        {
+            inputAction = playerInput.actions.FindAction("CastDown");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -48,7 +57,7 @@ public class playSirenGame : playFishingGame
         }
         else if (gameObject.tag == downBarTag)
         {
-            isUpSide = false; 
+            isDownSide = true;
         }
         
     }
